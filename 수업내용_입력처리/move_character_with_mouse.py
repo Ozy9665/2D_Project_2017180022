eb584ea5bf2b5363f759e72c5af47bc645b1555b
OJW <ozy9665@gmail.com>
2022-10-11 12:19:10 +0900

    move with key
    클래스 적용
    맵 그리기
    캐릭터 그리고 좌우이동

---
+++ 수업내용_입력처리/move_character_with_mouse.py
@@ -0,0 +1,31 @@
+from pico2d import *
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+
+def handle_events():
+    # fill here
+    pass
+
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+
+running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+frame = 0
+hide_cursor()
+
+while running:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+
+    handle_events()
+
+close_canvas()
+
+
+
+
